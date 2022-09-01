bcaa20e3cbdb33c396ccaa145a71716fcafccedb
Fengying Liu <bgj205@myy.haaga-helia.fi>
2022-09-01 13:07:43 +0300

    first

---
+++ src/main/java/com/example/MonicaBookStore/MonicaBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.MonicaBookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonicaBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonicaBookStoreApplication.class, args);
+	}
+
+}
