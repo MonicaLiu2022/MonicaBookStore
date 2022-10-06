46ea09e705fd34ef9a588179f2fb272892d3d936
Fengying Liu <bgj205@myy.haaga-helia.fi>
2022-10-06 13:30:19 +0300

    enhanced

--- src/main/java/com/example/MonicaBookStore/MonicaBookStoreApplication.java
+++ src/main/java/com/example/MonicaBookStore/MonicaBookStoreApplication.java
@@ -12,6 +12,9 @@ import com.example.MonicaBookStore.model.Book;
 import com.example.MonicaBookStore.model.BookRepository;
 import com.example.MonicaBookStore.model.Category;
 import com.example.MonicaBookStore.model.CategoryRepository;
+import com.example.MonicaBookStore.model.User;
+import com.example.MonicaBookStore.model.UserRepository;
+
 
 @SpringBootApplication
 public class MonicaBookStoreApplication {
@@ -22,7 +25,7 @@ public class MonicaBookStoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner demo(BookRepository brepository,CategoryRepository crepository) {
+	public CommandLineRunner demo(BookRepository brepository,CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			
 		log.info("save a couple of books");
@@ -34,6 +37,12 @@ public class MonicaBookStoreApplication {
 		Book book1 = new Book("A Farewell to Arms","Ernest Hemingway",1929,"1232323-21",15.23,crepository.findByName("Novel").get(0));
 		Book book2 = new Book("Animal Farm","George Orwell",1945,"2212343-5",16.00, crepository.findByName("Contemporary").get(0));
 		
+		// Create users: admin/admin user/user
+		User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+		User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+		urepository.save(user1);
+		urepository.save(user2);
+					
 		log.info("fetch all books");
 		for (Book book: brepository.findAll()) {
 			log.info(book.toString());
--- src/main/java/com/example/MonicaBookStore/web/BookController.java
+++ src/main/java/com/example/MonicaBookStore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,11 @@ import com.example.MonicaBookStore.model.CategoryRepository;
 	@Autowired
 	private CategoryRepository crepository;
 	
+	// Login page
+	@RequestMapping(value="/login")
+    public String login() {	
+        return "login";
+    }
 	// Show book list
 	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
 	public String getBookList(Model model) {
@@ -54,6 +60,7 @@ import com.example.MonicaBookStore.model.CategoryRepository;
     }
     // Delete book
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
      repository.deleteById(bookId);
      return "redirect:../booklist";
